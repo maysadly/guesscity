b7e69b5a834facd3245502cd8104a2ba9c22dc38
Tleukhan <maysadlysw@gmail.com>
2023-04-28 21:49:41 +0600

    bingchiling

---
+++ farm.py
@@ -0,0 +1,155 @@
+import sys
+import os
+import pygame
+import math
+import requests
+
+
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+def geocode(address):
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": API_KEY,
+        "geocode": address,
+        "format": "json"}
+
+    response = requests.get(geocoder_request, params=geocoder_params)
+
+    if response:
+        json_response = response.json()
+    else:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {geocoder_request}
+            Http статус: {response.status_code} ({response.reason})""")
+
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+
+def get_coordinates(address):
+    toponym = geocode(address)
+    if not toponym:
+        return None, None
+
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return float(toponym_longitude), float(toponym_lattitude)
+
+
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
+
+
+def find_businesses(ll, spn, request, locale="ru_RU"):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+    search_params = {
+        "apikey": api_key,
+        "text": request,
+        "lang": locale,
+        "ll": ll,
+        "spn": spn,
+        "type": "biz"
+    }
+
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {search_api_server}
+            Http статус: {response.status_code} ({response.reason})""")
+
+    json_response = response.json()
+
+    organizations = json_response["features"]
+    return organizations
+
+
+def find_business(ll, spn, request, locale="ru_RU"):
+    orgs = find_businesses(ll, spn, request, locale=locale)
+    if len(orgs):
+        return orgs[0]
+    
+
+def show_map(ll_spn=None, map_type="map", add_params=None):
+    if ll_spn:
+        map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
+    else:
+        map_request = f"http://static-maps.yandex.ru/1.x/?l={map_type}"
+
+    if add_params:
+        map_request += "&" + add_params
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
+    os.remove(map_file)
+
+
+def main():
+    toponym_to_find = " ".join(sys.argv[1:])
+
+    lat, lon = get_coordinates(toponym_to_find)
+    address_ll = f"{lat},{lon}"
+    span = "0.005,0.005"
+
+    organization = find_business(address_ll, span, "аптека")
+    point = organization["geometry"]["coordinates"]
+    org_lat = float(point[0])
+    org_lon = float(point[1])
+    point_param = f"pt={org_lat},{org_lon},pm2dgl"
+
+    show_map(f"ll={address_ll}&spn={span}", "map", add_params=point_param)
+
+    points_param = point_param + f"~{address_ll},pm2rdl"
+
+    show_map("ll={0}&spn={1}".format(address_ll, span), "map", add_params=points_param)
+
+    show_map(map_type="map", add_params=points_param)
+
+    name = organization["properties"]["CompanyMetaData"]["name"]
+    address = organization["properties"]["CompanyMetaData"]["address"]
+    time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+    distance = round(lonlat_distance((lon, lat), (org_lon, org_lat)))
+    snippet = f"Название:\t{name}\nАдрес:\t{address}\nВремя работы:\t{time}\n" \
+              f"Расстояние:\t{distance}м."
+    print(snippet)
+
+
+if __name__ == "__main__":
+    main()
